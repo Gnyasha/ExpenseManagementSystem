ce8e9370b5a3a00e5c8321b28890631f2ae62a3c
Goodson Nyasha Mutongi <goodnyasha@yahoo.co.uk>
2021-09-17 01:28:03 +0200

    Db Access changes

--- Application.Contracts/DatabaseSessions/IDbAccess.cs
+++ Application.Contracts/DatabaseSessions/IDbAccess.cs
@@ -9,10 +9,28 @@ namespace Application.Contracts.DatabaseSessions
 
     public interface IDbAccess
     {
-        /// <summary>
-        /// Returns all SystemUsers
-        /// </summary>
-        /// <returns></returns>
-        IQueryable<SystemUser> GetAllSystemUsers();
+
+        //Get list of entities
+        IQueryable<SystemUser> GetSystemUsers();
+        IQueryable<TransactionStatus> GetTransactionStatuses();
+        IQueryable<TransactionAccount> GetTransactionAccounts();
+        IQueryable<Transaction> GetTransactions();
+        IQueryable<SystemLog> GetSystemLogs();
+
+
+        // Save or Updates entity and returns the same entity
+        SystemUser SaveOrUpdate(SystemUser entity);
+        TransactionStatus SaveOrUpdate(TransactionStatus entity);
+        TransactionAccount SaveOrUpdate(TransactionAccount entity);
+        Transaction SaveOrUpdate(Transaction entity);
+        SystemLog SaveOrUpdate(SystemLog entity);
+
+
+        //Delete the provided entity
+        void DeleteEntity(SystemUser entity);
+        void DeleteEntity(TransactionStatus entity);
+        void DeleteEntity(TransactionAccount entity);
+        void DeleteEntity(Transaction entity);
+        void DeleteEntity(SystemLog entity);
     }
 }
--- Application.Resources/Data/DbAccess.cs
+++ Application.Resources/Data/DbAccess.cs
@@ -27,9 +27,89 @@ namespace Application.Resources.Data
             session = _session;
         }
 
-        public IQueryable<SystemUser> GetAllSystemUsers()
+        public void DeleteEntity(SystemUser entity)
+        {
+            session.Delete(entity);
+        }
+
+        public void DeleteEntity(TransactionStatus entity)
+        {
+            session.Delete(entity);
+        }
+
+        public void DeleteEntity(TransactionAccount entity)
+        {
+            session.Delete(entity);
+        }
+
+        public void DeleteEntity(Transaction entity)
+        {
+            session.Delete(entity);
+        }
+
+        public void DeleteEntity(SystemLog entity)
+        {
+            session.Delete(entity);
+        }
+
+        public IQueryable<TransactionStatus> GetTransactionStatuses()
+        {
+            return session.Query<TransactionStatus>();
+        }
+
+        public IQueryable<SystemLog> GetSystemLogs()
+        {
+            return session.Query<SystemLog>();
+        }
+
+        public IQueryable<SystemUser> GetSystemUsers()
         {
             return session.Query<SystemUser>();
+        }
+
+        public IQueryable<TransactionAccount> GetTransactionAccounts()
+        {
+            return session.Query<TransactionAccount>();
+        }
+
+        public IQueryable<Transaction> GetTransactions()
+        {
+            return session.Query<Transaction>();
+        }
+
+        public SystemUser SaveOrUpdate(SystemUser entity)
+        {
+            session.SaveOrUpdate(entity);
+            session.FlushAsync();
+            return entity;
+        }
+
+        public TransactionStatus SaveOrUpdate(TransactionStatus entity)
+        {
+            session.SaveOrUpdate(entity);
+            session.FlushAsync();
+            return entity;
+        }
+
+        public TransactionAccount SaveOrUpdate(TransactionAccount entity)
+        {
+            session.SaveOrUpdate(entity);
+            session.FlushAsync();
+            return entity;
+        }
+
+        public Transaction SaveOrUpdate(Transaction entity)
+        {
+            session.SaveOrUpdate(entity);
+            session.FlushAsync();
+            return entity;
+        }
+
+        public SystemLog SaveOrUpdate(SystemLog entity)
+        {
+            session.SaveOrUpdate(entity);
+            session.FlushAsync();
+            return entity;
         }
     }
 }
